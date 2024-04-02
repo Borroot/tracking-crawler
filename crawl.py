2d92c500f186c3d366f5eb59fb99d268f7fb61c8
robintup598 <82162374+robintup598@users.noreply.github.com>
2024-04-02 11:14:20 +0200

    Update crawl.py

--- crawl.py
+++ crawl.py
@@ -35,38 +35,7 @@ def read_lines_of_file(file_path):
             lines.append(line.strip())
     return lines
 
-
-def deny_trackers(playwright, url): 
-    browser = playwright.chromium.launch(headless=False, slow_mo=50)
-    context = browser.new_context()
-    sanitized_url = sanitize_url(url)
-    record_video_dir = "videos/"+sanitized_url+"/deny/"
-    context = browser.new_context(
-    record_video_dir=record_video_dir,
-    record_video_size={"width": 640, "height": 480}
-    )
-    page = context.new_page()
-    
-    page.goto(url)
-    # Wait 10s
-
-    # Accept all cookies, but block the trackers from the services.json list
-
-    # wait 3s
-
-    # Scroll all the way down, in multiple steps
-
-    # wait 3s
-
-    # Close the page
-
-    context.close()
-    
-    browser.close()
-    return "TODO"
-
-
-def accept_cookies(playwright, url):
+def accept_cookies(playwright, url, debug):
     browser = playwright.chromium.launch(headless=False, slow_mo=50)
     context = browser.new_context()
     sanitized_url = sanitize_url(url)
@@ -79,39 +48,112 @@ def accept_cookies(playwright, url):
     
     page.goto(url)
     # Wait 10s
+    if debug:
+        print("Waiting for 10 seconds")
+    page.wait_for_timeout(3000)
 
-    # Accept the cookies by clicking
+    # Accept all cookies
+    if debug:
+        print("Accepting all cookies")
+        # TODO: Implement this
 
     # wait 3s
+    if debug:
+        print("Waiting for 3 seconds")
+    page.wait_for_timeout(3000)
 
     # Scroll all the way down, in multiple steps
+    if debug:
+        print("Scrolling all the way down")
+        # TODO: Implement this
 
     # wait 3s
+    if debug:
+        print("Waiting for 3 seconds")
+    page.wait_for_timeout(3000)
 
     # Close the page
+
+    context.close()
+    
+    browser.close()
+    return "TODO"
+
+def deny_trackers(playwright, url, debug): 
+    browser = playwright.chromium.launch(headless=False, slow_mo=50)
+    context = browser.new_context()
+    sanitized_url = sanitize_url(url)
+    record_video_dir = "videos/"+sanitized_url+"/deny/"
+    context = browser.new_context(
+    record_video_dir=record_video_dir,
+    record_video_size={"width": 640, "height": 480}
+    )
+    page = context.new_page()
+    
+    page.goto(url)
+    # Wait 10s
+    if debug:
+        print("Waiting for 10 seconds")
+    page.wait_for_timeout(3000)
+
+    # Accept all cookies, but block the trackers from the services.json list
+    if debug:
+        print("Accepting all cookies and blocking trackers")
+    # TODO: Implement this
+
+
+    # wait 3s
+    if debug:
+        print("Waiting for 3 seconds")
+    page.wait_for_timeout(3000)
+
+    # Scroll all the way down, in multiple steps
+    if debug:
+        print("Scrolling all the way down")
+    # TODO: Implement this
+
+    # wait 3s
+    if debug:
+        print("Waiting for 3 seconds")
+    page.wait_for_timeout(3000)
+
+    # Close the page
+
+    context.close()
+    
+    browser.close()
     return "TODO"
 
 
+
+
+
 def main():
+    # python crawl.py -u "https://playwright.dev" --debug
     # Gather arguments in variables
     block_trackers, url, file_path, debug = parse_arguments()
-
+    urls = []
     # All the url's we will visit
-    nl_gov_sites = read_lines_of_file("utils/nl-gov-sites.txt")
+    if file_path is not None:
+        urls = read_lines_of_file(file_path)
 
+    if url is not None:
+        urls = [url]
 
     with sync_playwright() as playwright:
         # Browser part, we should do this for every nl_gov_sites/url there is
 
         # To debug and just use 1 website
-        nl_gov_sites = ["https://playwright.dev"]
-        print(url)
-        for url in tqdm(nl_gov_sites):
-            # Once for denying cookies
-            deny_trackers(playwright, url)
 
+        print(url)
+        for url_loop in tqdm(urls):
             # Once for accepting cookies
-            accept_cookies(playwright, url)
+            accept_cookies(playwright, url_loop, debug)
+
+            # Once for denying cookies
+            deny_trackers(playwright, url_loop, debug)
+
+
 
 
 
